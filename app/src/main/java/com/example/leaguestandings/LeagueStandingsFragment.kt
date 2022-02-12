2722d74bfde655431b2880cd1e8251d821891c7c
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-02-12 13:58:53 +0100

    Move spinner to fragment

--- app/src/main/java/com/example/leaguestandings/LeagueStandingsFragment.kt
+++ app/src/main/java/com/example/leaguestandings/LeagueStandingsFragment.kt
@@ -2,6 +2,9 @@ package com.example.leaguestandings
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -10,20 +13,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.leaguestandings.databinding.FragmentLeagueStandingsBinding
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-const val LEAGUE = 39
+var LEAGUE = 39
 const val SEASON = 2021
 
-class LeagueStandingsFragment : Fragment(R.layout.fragment_league_standings) {
+class LeagueStandingsFragment : Fragment(R.layout.fragment_league_standings),
+    AdapterView.OnItemSelectedListener {
 
     private lateinit var binding: FragmentLeagueStandingsBinding
 
-    lateinit var standingsAdapter: StandingsAdapter
+    private lateinit var standingsAdapter: StandingsAdapter
     private val standingsViewModel : StandingsViewModel by viewModels()
-    lateinit var teams: List<Standing>
+    private val leagueListViewModel : LeagueListViewModel by viewModels()
+    private lateinit var teams: List<Standing>
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,23 +35,33 @@ class LeagueStandingsFragment : Fragment(R.layout.fragment_league_standings) {
         binding = FragmentLeagueStandingsBinding.bind(view)
 
         setupRecyclerView()
+
+        val spinner = binding.leagueSpinner
+        adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.spinner_item,
+            leagueListViewModel.leagueList
+        )
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
+
         lifecycleScope.launch(Main) {
-            standingsViewModel.getStandings(LEAGUE, SEASON)
+            standingsViewModel.getStandings()
         }
 
-        val standingsObserver = Observer<List<Standing>> { newTeams ->
-            teams = newTeams
+        val standingsObserver = Observer<List<Standing>> { _ ->
             standingsAdapter.setListData(standingsViewModel.standings.value!!)
             standingsAdapter.notifyDataSetChanged()
         }
 
         standingsViewModel.standings.observe(viewLifecycleOwner, standingsObserver)
 
-        binding.button.setOnClickListener {
-            lifecycleScope.launch(IO) {
-                standingsViewModel.getStandings(135, 2021)
-                }
-        }
+//        binding.button.setOnClickListener {
+//            lifecycleScope.launch(IO) {
+//                standingsViewModel.leagueId = 140
+//                standingsViewModel.getStandings()
+//                }
+//        }
     }
 
     private fun setupRecyclerView() = binding.myRecyclerView.apply {
@@ -56,16 +70,28 @@ class LeagueStandingsFragment : Fragment(R.layout.fragment_league_standings) {
         layoutManager = LinearLayoutManager(context)
     }
 
-    suspend fun test() {
-        withContext(Main) {
-            teams = standingsViewModel.standings.value!!
-            setupRecyclerView()
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if (parent!!.selectedItemPosition == 0) {
+            lifecycleScope.launch(IO) {
+                standingsViewModel.leagueId = 39 // Premier League
+                standingsViewModel.getStandings()
+            }
+            Toast.makeText(context, "pressed ${parent.selectedItemPosition}", Toast.LENGTH_SHORT).show()
+        }
+        if (parent.selectedItemPosition == 1) {
+            lifecycleScope.launch(IO) {
+                standingsViewModel.leagueId = 140 // LaLiga
+                standingsViewModel.getStandings()
+            }
+        }
+        if (parent.selectedItemPosition == 2) {
+            lifecycleScope.launch(IO) {
+                standingsViewModel.leagueId = 135 // Serie A
+                standingsViewModel.getStandings()
+            }
         }
     }
 
-    suspend fun test1() {
-        withContext(Main) {
-            teams = standingsViewModel.standings.value!!
-        }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
     }
 }
--- app/src/main/java/com/example/leaguestandings/StandingsViewModel.kt
+++ app/src/main/java/com/example/leaguestandings/StandingsViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.ViewModel
 class StandingsViewModel : ViewModel() {
 
     var standings = MutableLiveData<List<Standing>>()
+    var leagueId = 39
+    var season = 2021
 
-    suspend fun getStandings(league: Int, season: Int) {
-        val response = StandingsRepository(RetrofitInstance.api).getStandings(league, season)
+    suspend fun getStandings() {
+        val response = StandingsRepository(RetrofitInstance.api).getStandings(leagueId, season)
 
         standings.postValue(mutableListOf(response.body()!!.response[0].league.standings[0]).flatten())
 //        println(standings.value)
