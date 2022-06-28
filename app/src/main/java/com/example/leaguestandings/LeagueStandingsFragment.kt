9c6e2505b217c3b3fb6baddc57af951f748fe7b1
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-06-28 18:54:59 +0200

    optimmize imports

--- app/src/main/java/com/example/leaguestandings/LeagueStandingsFragment.kt
+++ app/src/main/java/com/example/leaguestandings/LeagueStandingsFragment.kt
@@ -3,18 +3,11 @@ package com.example.leaguestandings
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.leaguestandings.databinding.FragmentLeagueStandingsBinding
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
 
 class LeagueStandingsFragment : Fragment(R.layout.fragment_league_standings) {
 
